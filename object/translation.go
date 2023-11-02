305eaf7623fe019935bbd83b7b0f3d95f4dd09f9
Ben Pate <ben@pate.org>
2023-11-02 12:58:07 -0600

    Update Translation object

--- object/translation.go
+++ object/translation.go
@@ -2,4 +2,7 @@ package object
 
 // https://docs.joinmastodon.org/entities/Translation/
 type Translation struct {
+	Content                string `json:"content"`                  // HTML: The translated text of the status.
+	DetectedSourceLanguage string `json:"detected_source_language"` // ISO 639 Language Code: The language of the source text, as auto-detected by the machine translation provider.
+	Provider               string `json:"provider"`                 // The service that provided the machine translation.
 }
